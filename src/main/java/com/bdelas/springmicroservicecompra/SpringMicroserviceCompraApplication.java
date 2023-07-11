057d0de097eb78bc1e4520bccfd9c24401b3a92e
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-07-11 17:46:20 -0600

    Inicio

---
+++ src/main/java/com/bdelas/springmicroservicecompra/SpringMicroserviceCompraApplication.java
@@ -0,0 +1,13 @@
+package com.bdelas.springmicroservicecompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceCompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdelas/springmicroservicecompra/SpringMicroserviceCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdelas.springmicroservicecompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
